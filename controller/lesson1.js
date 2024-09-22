40bdbd8a3c8bca4082f1ac1736bf13792896c588
Berny Fred <fredbern7@gmail.com>
2024-09-22 17:50:39 +1100

    week one

---
+++ controller/lesson1.js
@@ -0,0 +1,14 @@
+const emilyRoute = (req, res) => {
+    res.send('Emily Birch');
+};
+
+const hannaRoute = (req, res) => {
+    res.send('Hannah Birch');
+};
+
+module.exports = {
+    emilyRoute,
+    hannaRoute
+}
+
+
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // express web server
 const express = require('express');
+const { emilyRoute, hannaRoute } = require('./controller/lesson1');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Emily Birch');
-});
+
+app.get('/', emilyRoute);
+
+app.get('/hanna', hannaRoute);
 
 const port = 3000;
 
 app.listen(process.env.port || port);
-console.log('Web Server is listening at port ' * (process.env.port || port));
+console.log(`Web Server is listening at port ${(process.env.port || port)}`);
